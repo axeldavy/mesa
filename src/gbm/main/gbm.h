11c0351d6148089db7ffce3a99096f9ea3cf9120
Kristian Høgsberg <krh@bitplanet.net>
2014-03-28 17:17:11 +0000

    gbm: Add import from fd
    Signed-off-by: Axel Davy <axel.davy@ens.fr>

--- src/gbm/main/gbm.h
+++ src/gbm/main/gbm.h
@@ -233,6 +233,14 @@ gbm_bo_create(struct gbm_device *gbm,
 #define GBM_BO_IMPORT_WL_BUFFER         0x5501
 #define GBM_BO_IMPORT_EGL_IMAGE         0x5502
 
+#define GBM_BO_IMPORT_FD                0x5503
+
+struct gbm_import_fd_data {
+   int fd;
+   uint32_t width, height, stride;
+   uint32_t format;
+};
+
 struct gbm_bo *
 gbm_bo_import(struct gbm_device *gbm, uint32_t type,
               void *buffer, uint32_t usage);
